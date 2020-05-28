193a35f7c2e119d2d6b4c8af0626167e9fe28be1
mye0ngs00 <ourendpoint@gmail.com>
2020-05-28 16:25:49 +0900

    ~134p

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-import EventPractice from './EventPractice';
+import ValidationSample from './ValidationSample';
 
 function App() {
   return (
-    <EventPractice/>
+    <ValidationSample/>
   );
 }
 
