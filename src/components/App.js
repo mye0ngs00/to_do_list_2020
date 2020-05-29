d33b506c11c4ff3f65eab320af89d79fadb7f2a3
mye0ngs00 <ourendpoint@gmail.com>
2020-05-29 17:14:37 +0900

    ~201p

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import PageTemplate from './PageTemplate';
 
 class App extends Component{
   render() {
     return (
       <div>
-        일정 관리
+        <PageTemplate />
       </div>
     )
   }
