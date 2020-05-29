36c1598f491aa9bf596a794d46913eaa579278db
mye0ngs00 <ourendpoint@gmail.com>
2020-05-29 16:56:42 +0900

    ~p196 reset

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './components/App';
+import './styles/main.scss';
+import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
