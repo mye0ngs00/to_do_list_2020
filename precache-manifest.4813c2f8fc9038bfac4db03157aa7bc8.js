492c1fff1c8a05a51cd4a22fe399b6c488295875
mye0ngs00 <ourendpoint@gmail.com>
2020-05-31 14:52:43 +0900

    Updates

---
+++ precache-manifest.4813c2f8fc9038bfac4db03157aa7bc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "631098f57e13fd758c11d2a39f8e92b9",
+    "url": "/to_do_list_2020/index.html"
+  },
+  {
+    "revision": "1e71c3d28939db01134a",
+    "url": "/to_do_list_2020/static/css/main.0fc26a1a.chunk.css"
+  },
+  {
+    "revision": "c83474d18b79be70e9e2",
+    "url": "/to_do_list_2020/static/js/2.67109aaa.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/to_do_list_2020/static/js/2.67109aaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e71c3d28939db01134a",
+    "url": "/to_do_list_2020/static/js/main.5fe5df58.chunk.js"
+  },
+  {
+    "revision": "415a0f42a146beb661c2",
+    "url": "/to_do_list_2020/static/js/runtime-main.f7e16973.js"
+  }
+]);
